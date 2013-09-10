ba191c917684d8e9d0367fd94003b62b56109073
Jonathan Balsano <jrbalsano@gmail.com>
2013-09-10 19:12:15 -0400

    Added heuristics for when to shoot and who to shoot

--- gunslinger/G4PlayerV1/Player.java
+++ gunslinger/G4PlayerV1/Player.java
@@ -1,4 +1,4 @@
-package gunslinger.dumb;
+package gunslinger.G4PlayerV1;
 
 import java.util.*;
 
@@ -12,14 +12,26 @@ public class Player extends gunslinger.sim.Player
     // my version no
     private int version = versions++;
     
+    private static String PLAYER_NAME="G4PlayerV1";
+
     // A simple fixed shoot rate strategy used by the dumb player
     private static double ShootRate = 0.8;
+
+    private int e;
+    private int f;
+    private int potential;
+    private int current;
+
+    // Matrix mapping shots by x at y
+    private int[][] shotsFired;
+    private int[] shotAt;
+    private int round;
 
     // name of the team
     //
     public String name()
     {
-        return "dumb" + (versions > 1 ? " v" + version : "");
+        return PLAYER_NAME + (versions > 1 ? " v" + version : "");
     }
  
     // Initialize the player
@@ -37,8 +49,15 @@ public class Player extends gunslinger.sim.Player
         // gen = new Random(seed);
 
         this.nplayers = nplayers;
+        this.e = enemies.length;
+        this.f = friends.length;
+        this.potential = e+f+1;
+        this.current = f+1;
         this.friends = friends.clone();
         this.enemies = enemies.clone();
+        this.shotsFired = new int[nplayers][nplayers];
+        this.shotAt = new int[nplayers];
+        this.round = 0;
     }
 
     // Pick a target to shoot
@@ -54,22 +73,53 @@ public class Player extends gunslinger.sim.Player
         /* Strategy used by the dumb player:
            Decide whether to shoot or not with a fixed shoot rate
            If decided to shoot, randomly pick one alive that is not your friend */
-                
+
+        round++;
+
+        for (int i = 0; i < nplayers; i++) {
+          if (prevRound != null && prevRound[i] >= 0) {
+            shotsFired[i][prevRound[i]]++;
+            shotAt[prevRound[i]]++;
+          }
+        }
+
         // Shoot or not in this round?
-        boolean shoot = gen.nextDouble() < ShootRate;
+        current = 1;
+        potential = 1;
+        for(int i = 0; i < nplayers; i++) {
+          if (alive[i] && Arrays.asList(friends).contains(i)) {
+            current++;
+            potential++;
+          }
+          else if (!alive[i] && Arrays.asList(enemies).contains(i)) {
+            current++;
+            potential++;
+          }
+          else if (alive[i] && Arrays.asList(enemies).contains(i)) {
+            potential++;
+          }
+        }
+
+        boolean shoot = current/potential < .5;
 
         if (!shoot)
             return -1;
+        
 
-        ArrayList<Integer> targets = new ArrayList<Integer>();
-        for (int i = 0; i != nplayers; ++i)
-            if (i != id && alive[i] && !Arrays.asList(friends).contains(i))
-                targets.add(i);
 
-        int target = targets.get(gen.nextInt(targets.size()));
+        int max = 0;
+        int target = -1;
+        for (int i = 0; i != nplayers; ++i) {
+            if (i != id && alive[i] && !Arrays.asList(friends).contains(i)) {
+                if (max < shotAt[i]) {
+                  max = shotAt[i];
+                  target = i;
+                }
+            }
+        }
+
         return target;
     }
-
 
     private Random gen;
     private int nplayers;
