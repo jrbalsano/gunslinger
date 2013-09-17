c9305e1f4026ad542742b00ef02f8aba1342ed6e
Jonathan Balsano <jrbalsano@gmail.com>
2013-09-17 19:23:53 -0400

    Removed old version

--- gunslinger/g4jb/RoundListener.java
+++
@@ -1,8 +0,0 @@
-package gunslinger.g4jb;
-
-public interface RoundListener {
-	/**
-	 * Called by a GameHistory object when a new round has been created and saved.
-	 */
-	public void onNewRound(GameHistory history);
-}
